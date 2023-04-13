6a190503ff912038841a015275e9657c9540053d
Kler <klera@klera.ru>
2023-04-13 17:55:01 +0300

    New routes for Blog - list all, lust of current user, view, create, update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+     Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::group([], function(){ 
+        Route::get('/',[BlogController::class, 'index'])->name('main');         
+        Route::get('/blogpost/{blog}', [BlogController::class, 'view'])->name('blogs.view'); 
+    });
+ 
 
-require __DIR__.'/auth.php';
+    Route::middleware(['auth', 'verified'])->group(function(){
+        Route::get('/blogadmin',[BlogController::class, 'adminview'])->name('blogadmin');
+    
+        Route::get('/blogpost',[BlogController::class, 'add'])->name('blogs.add');
+        Route::post('/blogpost',[BlogController::class, 'create'])->name('blogs.create');
+         
+        Route::get('/blogpost/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
+        Route::post('/blogpost/{blog}', [BlogController::class, 'update'])->name('blogs.update');
+    });
+ 
+ 
+
+
+
+    require __DIR__.'/auth.php';
